939b6a018fff3932d27fdcccab5d1d2b1f579112
Malissa Bishop <malissambishop7@gmail.com>
2021-10-14 16:01:55 -0700

    commit tree 95d5a3a7e1abec9c156e85848cf9a56e5fd69a94

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+// array of drivers
+function findMatching(array, string) {
+    const matchingList = array.filter(function(driver){
+       return (driver.toUpperCase() === string.toUpperCase())
+    })
+
+    return matchingList
+}
+
+function fuzzyMatch(array, string) {
+    const match = array.filter(function(driver) {
+        return (driver.charAt() === string.charAt())
+    })
+    return match
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
